c4172d1fdb1a1c51e080b327684d913e872ad4f6
Hamed Rezaee <13097270+hamed-rezaee@users.noreply.github.com>
2020-12-18 22:45:28 +0330

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_battle_ship/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_battle_ship
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_battle_ship/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Battle Ship',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const MyHomePage(),
+      );
+}
